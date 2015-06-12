7090f288ede9267959e68c30e76f8620b9a01f43
nick weinberg <nickweinberg90@gmail.com>
2015-06-12 14:46:03 -0700

    update gitignore

---
+++ src/main.js
@@ -0,0 +1,88 @@
+/** @jsx React.DOM */
+// ... React components inside here
+var Chart = React.createClass({
+  render: function() {
+    return (
+      <svg
+        width={this.props.width}
+        height={this.props.height}>
+        {this.props.children}
+      </svg>
+    );
+  }
+});
+
+var Bar = React.createClass({
+  getDefaultProps: function() {
+    return {
+      width: 0,
+      height: 0,
+      offset: 0
+    }
+  },
+  render: function() {
+    return (
+      <rect
+        fill={this.props.color}
+        width={this.props.width}
+        height={this.props.height}
+        x={this.props.offset}
+        y={this.props.availableHeight - this.props.height} />
+      );
+  }
+});
+
+var DataSeries = React.createClass({
+  getDefaultProps: function() {
+    return {
+      title: '',
+      data: []
+    }
+  },
+  render: function() {
+    var props = this.props;
+
+    var yScale = d3.scale.linear()
+                   .domain([0, d3.max(this.props.data)])
+                   .range( [0, this.props.height]);
+
+
+    var xScale = d3.scale.ordinal()
+                   .domain(d3.range(this.props.data.length))
+                   .rangeRoundBands([0, this.props.width], 0.05);
+
+    var bars = _.map(this.props.data, function(point, i) {
+      console.log(yScale(point));
+      return (
+        <Bar
+          height={yScale(point)}
+          width={xScale.rangeBand()}
+          offset={xScale(i)}
+          availableHeight={this.props.height}
+          color={props.color}
+          key={i} />
+        );
+    });
+
+    return (<g>{bars}</g>);
+  }
+});
+
+var BarChart = React.createClass({
+  render: function() {
+    return (
+      <Chart width={this.props.width} height={this.props.height}>
+        <DataSeries
+          data={chartData}
+          width={this.props.width}
+          height={this.props.height}
+          color={chartColor} />
+      </Chart>
+    );
+  }
+});
+
+React.render(
+  <BarChart width={600} height={300} />,
+  document.getElementById('container')
+);
